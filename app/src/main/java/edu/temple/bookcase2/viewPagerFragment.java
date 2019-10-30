0ecd668e340bcc1e509f3dd2dc58faaa44e05f0a
JoshuaKang1997 <joshkang1997@gmail.com>
2019-10-29 22:08:13 -0400

    added files for viewPagerFragments

---
+++ app/src/main/java/edu/temple/bookcase2/viewPagerFragment.java
@@ -0,0 +1,26 @@
+package edu.temple.bookcase2;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class viewPagerFragment extends Fragment {
+    
+    public static viewPagerFragment newInstance() {
+        viewPagerFragment fragment = new viewPagerFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_view_pager, container, false);
+    }
+
+}
