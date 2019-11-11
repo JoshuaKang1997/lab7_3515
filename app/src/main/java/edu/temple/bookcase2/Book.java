49334d6bf403ad957ceac93d0169e01c4bba183f
JoshuaKang1997 <joshkang1997@gmail.com>
2019-11-11 04:17:05 -0500

    added new branch and book.java

---
+++ app/src/main/java/edu/temple/bookcase2/Book.java
@@ -0,0 +1,23 @@
+package edu.temple.bookcase2;
+
+import android.content.Context;
+
+public class Book {
+
+    Context context;
+    private int id;
+    private String title;
+    private String author;
+    private int year;
+    private String coverURL;
+
+    public  Book(int id, String title, String author, int year, String coverURL, Context context){
+        this.context = context;
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.coverURL = coverURL;
+    }
+
+}
